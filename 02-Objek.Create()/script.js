a5099ce8bf192a4b93868e61672ffefafaf9246f
dianynf <dianbugas@gmail.com>
2019-12-28 11:23:15 +0700

    objek,create

---
+++ 02-Objek.Create()/script.js
@@ -0,0 +1,90 @@
+//cara membuat obejek pada javascript
+//----tidak efektif untuk projek yang banyak
+
+//----------1 Objek Literal---------
+// let mahasiswa = {
+//     //properti
+//     nama: 'dian',
+//     energi: 10,
+//     //method
+//     makan: function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Helo ${this.nama}, Selamat Makan`);
+//     }
+// }
+
+// let mahasiswa1 = {
+//     //properti
+//     nama: 'bugas',
+//     energi: 20,
+//     //method
+//     makan: function (porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Helo ${this.nama}, Selamat Makan`);
+//     }
+// }
+
+
+//---------2 Function deklaration----------
+//tidak perlu di duplikat seperti objek literal
+//lebih efektif lagi cuma satu kali di simpan methodnya di memori
+const methodMahasiswa = {
+    //method
+    makan: function (porsi) {
+        this.energi += porsi;
+        console.log(`halo ${this.nama}, selamat makan`);
+    },
+
+    main: function (jam) {
+        this.energi -= jam;
+        console.log(`halo ${this.nama} selamat bermain`);
+    },
+
+    tidur: function (jam) {
+        this.energi += jam * 2;
+        console.log(`halo ${this.nama}, selamat tidur`);
+    }
+};
+
+function Mahasiswa(nama, energi) { //templete
+    let mahasiswa = Object.create(methodMahasiswa); //beda dgn yang lain ini lebih efektif dan bisa menyimpan objek di dalam nya. dan membawa objek methodMahasiswa YG DIKEDAL DENGAN PEWARISAN
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    //------kelemahan mengelola dua objek jadi di hapus dan ada cara otomatis nnya
+    // mahasiswa.makan = methodMahasiswa.makan;
+    // mahasiswa.main = methodMahasiswa.main;
+    // mahasiswa.tidur = methodMahasiswa.tidur;
+    return mahasiswa; //objek
+}
+
+//objek
+let bugas = Mahasiswa('bugas', 10);
+let dian = Mahasiswa('dian', 12);
+
+
+
+
+//3 Construktor Function
+//keywprd new (sering di gunakan) tidak perlu deklarasi variabel dan ruturn nya karena otomatis
+
+// function Mahasiswa(nama, energi) { //templete
+//     this.nama = nama;
+//     this.energi = energi;
+
+//     //method
+//     this.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`halo ${this.nama}, selamat makan`);
+//     }
+
+//     this.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`halo ${this.nama} selamat bermain`);
+//     }
+// }
+
+// let dian = new Mahasiswa('dian', 10);
+
+
+
+//4 Objek create
